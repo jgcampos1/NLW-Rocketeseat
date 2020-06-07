31a21286d196a43f965d04e8e39050082c2ea4f1
JG <joaogabriel.oi@hotmail.com>
2020-06-07 14:24:08 -0300

    adicionando bando de dados

---
+++ src/database/db.js
@@ -0,0 +1,83 @@
+//imporatando dependencia sqlite
+const sqlite3 = require("sqlite3").verbose()
+//criar obj que ira fazer operações no database
+const db = new sqlite3.Database("./src/database/database.db")
+
+
+module.exports = db
+//ultilizar o obj de database para oprações
+
+
+/*
+db.serialize(() => {
+
+  //usando comandos SLG
+  //criar uma tabela 
+  db.run(`
+    CREATE TABLE IF NOT EXISTS places (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      image TEXT,
+      name TEXT,
+      address TEXT,
+      address2 TEXT,
+      state TEXT,Resíduos Eletrônicos, Lâmpadas
+      city TEXT,
+      items TEXT            
+    );
+  `)
+  //inserir dados na tabela
+  const query = `
+  INSERT INTO places(
+    image,
+    name,
+    address,
+    address2,
+    state,
+    city,
+    items
+  )VALUES(?,?,?,?,?,?,?)
+`;
+  const values = [
+    "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    "Papersider",
+    "Guilherme Gemballa,Jardim America",
+    "Numero:260",
+    "Santa Catarina",
+    "Rio do Sul",
+    "Papéis e Papelão"
+  ];
+
+  function afterInsertData(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Cadastrado com sucess")
+    console.log(this)
+  }
+
+  db.run(query, values, afterInsertData);
+
+  //pesquisar dados
+  
+  db.all(`SELECT * FROM places`, function (err, rows) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Aqui estão seus registros");
+    console.log(rows);
+
+
+  })
+  
+  //delet dados
+  db.run(`DELETE FROM places WHERE id=?`, [3],function(err){
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Registro deletado com sucess");
+    
+  })
+  
+  
+})
+ */
--- src/server.js
+++ src/server.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const server = express();
+const db = require("./database/db")
 
 server.use(express.static("public"))
 
+const nunjucks=require("nunjucks")
+nunjucks.configure('src/views',{
+    express:server,
+    noCache:true
+})
+
 server.get("/",(req,res)=>{
-  res.sendFile(__dirname +"/views/index.html")
+  return res.render("index.html")
+})
+
+server.get("/create-point",(req,res)=>{
+  return res.render("create-point.html")
+})
+server.get("/search",(req,res)=>{
+  db.all(`SELECT * FROM places`, function (err, rows) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Aqui estão seus registros");
+    console.log(rows);
+    
+    const totalElements = rows.length 
+
+    return res.render("search-result.html",{places:rows,totalElements})
+  })
 })
 
 server.listen(3000)
